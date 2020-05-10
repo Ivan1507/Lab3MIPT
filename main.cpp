f0d6c28d9df73b235b4a13ab9c4e7b511fbcbf13
Ivan <iancurtis1507@mail.ru>
2020-05-10 16:54:21 +0300

    a xuy tam

--- main.cpp
+++ main.cpp
@@ -9,20 +9,26 @@ int main()
  int n,k;
  cin>>n>>k;
  int A[n];
+ int K[k];
  int max=0;
  for(int i=0;i<n;i++){
     cin>>A[i];
     max=A[1]-A[0];
  }
+ K[0]=A[0];
+ K[k-1]=A[n-1];
  int count=0;
+ int min_rast=1;
 for(int i=0;i<n-1;i++){
-    if(A[i+1]+A[i]>max){
-        count+=1;
-        max=A[i+1]-A[0];
-        continue;
-    }
+     if(A[i]+A[0]>max){
+        K[count+1]=A[i];
+     }
 }
- cout<<max;
+int max2=K[1]-K[0];
+for(int i=0;i<k-1;i++){
+    if(K[i+1]-K[i]>max2) max2=K[i+1]-K[i];
+}
+ cout<<K[n-1];
 }
 
 
