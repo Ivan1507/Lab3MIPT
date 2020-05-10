eea39f2fca06fd7c41d33505ad48dac701decb5c
Ivan <iancurtis1507@mail.ru>
2020-05-10 16:25:40 +0300

    testgit

--- main.cpp
+++ main.cpp
@@ -9,15 +9,20 @@ int main()
  int n,k;
  cin>>n>>k;
  int A[n];
+ int max=0;
  for(int i=0;i<n;i++){
     cin>>A[i];
+    max=A[1]-A[0];
  }
- int l=0;
- if(k>2){
-    l=A[n/k]-A[0];
- }
- else l=A[n-1]-A[0];
- cout<<l;
+ int count=0;
+for(int i=0;i<n-1;i++){
+    if(A[i+1]+A[i]>max){
+        count+=1;
+        max=A[i+1]-A[0];
+        continue;
+    }
+}
+ cout<<max;
 }
 
 
