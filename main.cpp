dcca26c90b997248b8ac92a14ddfb343273c0e46
Ivan <iancurtis1507@mail.ru>
2020-05-10 15:51:52 +0300

    Заботливый фермер

--- main.cpp
+++ main.cpp
@@ -6,19 +6,18 @@ using namespace std;
 
 int main()
 {
-
- string tek,bud;
- cin>>tek>>bud;
- int sum=0;
- sum=((tek[0]-48)*10+(tek[1]-48))*3600+((tek[3]-48)*10+(tek[4]-48))*60+((tek[6]-48)*10+(tek[7]-48));
-if(((bud[0]-48)*10+bud[1])<((tek[0]-48)*10+tek[1])){
-   bud[0]+=2;
-   bud[1]+=4;
-   }
-int sum1=0;
-sum1=((bud[0]-48)*10+(bud[1]-48))*3600+((bud[3]-48)*10+(bud[4]-48))*60+((bud[6]-48)*10+(bud[7]-48));
-int res=sum1-sum;
-cout<<res;
+ int n,k;
+ cin>>n>>k;
+ int A[n];
+ for(int i=0;i<n;i++){
+    cin>>A[i];
+ }
+ int l=0;
+ if(k>2){
+    l=A[n/k]-A[0];
+ }
+ else l=A[n-1]-A[0];
+ cout<<l;
 }
 
 
