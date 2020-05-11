3749559bb61763cb7e0749da9c7504021d5d679b
Ivan <iancurtis1507@mail.ru>
2020-05-11 10:21:53 +0300

    still solving

--- main.cpp
+++ main.cpp
@@ -1,26 +1,50 @@
-#include <iostream>
-#include<stdio.h>
-#include <iostream>
+ #include <iostream>
 #include <string>
 using namespace std;
-
 int main()
 {
- int n,k;
- cin>>n>>k;
- int A[n];
- for(int i=0;i<n;i++){
-    cin>>A[i];
- }
- int l=0;
- if(k>2){
-    l=A[n/k]-A[0];
- }
- else l=A[n-1]-A[0];
- cout<<l;
+
+ int n;
+ bool news;
+cin>>n>>news;
+string A[n];
+int den;
+bool B[n];
+int top=0;
+string dye[top+1];
+bool dy[top+1];
+for(int i=0;i<n;i++){
+cin>>A[i]>>B[i];
 }
+bool tek_news=news;
+cin>>den;
+int i=0;
 
+while(den!=0){
+        if(top==n-1) break;
+if(B[i]==1&&tek_news==0){
+    tek_news=0;
+    top++;
+    }
+      else if(B[i]==0&&tek_news==0) {
 
+tek_news=1;
+B[i]=true;
+}
+else if(B[i]==1&&tek_news==1) {
+        tek_news=1;
+
+}
+else if(B[i]==0&&tek_news==1){
+        tek_news=0;
+}
+    den--;
+    i++;
+}
+    for(int i=0;i<n;i++){
+        cout<<A[i]<<" "<<B[i]<<endl;
+    }
+ }
 
 
 
