f81ebdaddfa2416bd411e8126fa76b76c1a6f7a0
Ivan <iancurtis1507@mail.ru>
2020-05-19 15:22:11 +0300

    Вавиолнская система счисления

---
+++ Vavilon/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+ #include <math.h>
+using namespace std;
+
+int main()
+{
+ int n;
+ cin>>n;
+ if(n<0){
+        n=-n;
+ cout<<"-";
+ }
+ int k;
+ int e;
+ int l;
+ int c=60;
+ bool f=false;
+ int i=100;
+ int year=0;
+ while(i!=0){
+ year=n/pow(c,i);
+ if(year>=1){
+        f=true;
+        n=n-year*pow(c,i);
+        k=year/10;
+        while(k!=0){
+        cout<<"<";
+        k--;
+    }
+    e=year%10;
+    while(e!=0){
+        cout<<"v";
+        e--;
+    }
+    cout<<".";
+
+ }
+ else if(f==1) cout<<".";
+ i--;
+ }
+    k=n/10;
+    e=n%10;
+ while(k!=0){
+    cout<<"<";
+    k--;
+ }
+ while(e!=0){
+    cout<<"v";
+    e--;
+ }
+
+}
+
