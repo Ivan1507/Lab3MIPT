c561b06830f5965cdd25035ea91581bc67028dc0
Ivan <iancurtis1507@mail.ru>
2020-05-20 18:38:54 +0300

    not like bubble sort, but it works

--- Vavilon/main.cpp
+++ Vavilon/main.cpp
@@ -1,48 +1,46 @@
-#include <iostream>
- #include <math.h>
+ #include <iostream>
 using namespace std;
-
+void print(int A[],int n){
+for(int i=0;i<n;i++){
+    cout<<A[i]<<" ";
+}
+}
 int main()
 {
- int n;
- cin>>n;
- int k;
- int e;
- int c=60;
- bool f=false;
- int i=100;
- int year=0;
- while(i!=0){
- year=n/pow(c,i);
- if(year>=1){
-        f=true;
-        n=n-year*pow(c,i);
-        k=year/10;
-        while(k!=0){
-        cout<<"<";
-        k--;
+    int n;
+    cin>>n;
+    int A[n];
+    for(int i=0;i<n;i++){
+        cin>>A[i];
     }
-    e=year%10;
-    while(e!=0){
-        cout<<"v";
-        e--;
-    }
-    cout<<".";
+    int index=0;
+    int min=0;
+    for(int i=0;i<n;i++){
+        if(A[i]<0){
+            for(int j=0;j<n;j++){
+                if(A[i]>A[j]){
+                    int t=A[i];
+                    A[i]=A[j];
+                    A[j]=t;
+                    print(A,n);
+                    cout<<'\n';
+                }
+                }
 
- }
- else if(f==1) cout<<".";
- i--;
- }
-    k=n/10;
-    e=n%10;
- while(k!=0){
-    cout<<"<";
-    k--;
- }
- while(e!=0){
-    cout<<"v";
-    e--;
- }
+            }
+        else if(A[i]>0){
+            for(int j=0;j<n;j++){
+                if(A[j]>A[i]){
+                    int t=A[i];
+                    A[i]=A[j];
+                    A[j]=t;
+                    print(A,n);
+                    cout<<'\n';
+                }
+            }
+        }
+    }
+
+
 
 }
-
